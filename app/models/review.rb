238a88c212682d9a83c04eb7393f60d3ba155f90
Antoine Welter <tunwelter@gmail.com>
2018-11-28 15:46:11 +0000

    basis set

---
+++ app/models/review.rb
@@ -0,0 +1,6 @@
+class Review < ApplicationRecord
+  belongs_to :restaurant
+
+  validates :rating, presence: true, numericality: true, inclusion:{in: [0, 1, 2, 3, 4, 5]}
+  validates :content, presence: true
+end
